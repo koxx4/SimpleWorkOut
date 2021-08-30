f9a79d23a396e3b083768308a64070b4debee6ac
koxx4 <piotr815@protonmail.com>
2021-08-30 18:34:10 +0200

    proper hello world :)

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,12 @@
 import * as bootstrap from "bootstrap";
+import "core-js";
+import "regenerator-runtime";
 
+const helloButton = document.querySelector("button");
+const hiddenDivHello = document.querySelector(".visually-hidden");
+const clickedBefore = false;
+
+helloButton.addEventListener("click", (event) => {
+    hiddenDivHello.classList.toggle("visually-hidden");
+})
 
