6aeaa870ac5d37b0a20f87eb87debf66a7a785d0
koxx4 <piotr815@protonmail.com>
2021-08-31 14:59:34 +0200

    draft of the main page

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,21 @@
 import * as bootstrap from "bootstrap";
 import "core-js";
 import "regenerator-runtime";
+import Leaflet from "leaflet";
 
-const helloButton = document.querySelector("button");
-const hiddenDivHello = document.querySelector(".visually-hidden");
-const clickedBefore = false;
+let map = Leaflet
+    .map('map')
+    .setView([51.505, -0.09], 13);
 
-helloButton.addEventListener("click", (event) => {
-    hiddenDivHello.classList.toggle("visually-hidden");
-})
+Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoia294eDQiLCJhIjoiY2t0MDBtbWw0MDhmNjJvcXQ3Z3d3MHBnNSJ9.r5HuTy5Sq1H49DD4fit1tA',
+}).addTo(map);
 
+Leaflet.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
