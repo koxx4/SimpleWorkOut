140b8deebd14239bc11c990459e7fcba553aaab9
koxx4 <piotr815@protonmail.com>
2021-08-31 15:06:16 +0200

    draft of the main page

--- src/js/app.js
+++ src/js/app.js
@@ -3,19 +3,12 @@ import "core-js";
 import "regenerator-runtime";
 import Leaflet from "leaflet";
 
-let map = Leaflet
-    .map('map')
-    .setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09], 13);
 
-Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoia294eDQiLCJhIjoiY2t0MDBtbWw0MDhmNjJvcXQ3Z3d3MHBnNSJ9.r5HuTy5Sq1H49DD4fit1tA',
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-Leaflet.marker([51.5, -0.09]).addTo(map)
+L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
