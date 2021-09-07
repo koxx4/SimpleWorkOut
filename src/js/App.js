f2056b0753670202ca27f1d40e2fb6fe22c84019
koxx4 <piotr815@protonmail.com>
2021-09-07 22:40:58 +0200

    hotfix

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,7 @@ import "core-js";
 import "regenerator-runtime";
 import "../scss/style.scss";
 import "leaflet/dist/leaflet.css";
-import { initializeLeafletMap, LeafletMap } from "./LeafletMap";
+import { LeafletMap } from "./LeafletMap";
 import { Fader } from "./Fader";
 
 export class App {
