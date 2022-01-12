b3c858e8e13b1cf2f3247e5c2667243a2105f75a
koxx4 <piotr815@poczta.fm>
2022-01-12 11:02:55 +0100

    login error handling

--- src/js/controller/loginController.js
+++ src/js/controller/loginController.js
@@ -10,16 +10,16 @@ class LoginController {
     registerEventHandlers() {
         loginView.addEventListenerLoginSubmitButton("click", (event) => {
             event.preventDefault();
-            this.#loadUserProfileData(loginView.getLoginFormData()).then(() =>
-                this.#redirectToUserProfilePage()
-            );
+            this.#loadUserProfileData(loginView.getLoginFormData())
+                .then(() => this.#redirectToUserProfilePage())
+                .catch((reason) => this.#handleLoginError(reason.message));
         });
     }
 
     #loadUserProfileData(loginFormData) {
         const username = loginFormData.get("username");
         const password = loginFormData.get("password");
-        console.log(username + " | " + password);
+
         const httpBasicHeaderValue =
             "Basic " +
             Buffer.from(`${username}:${password}`).toString("base64");
@@ -33,24 +33,34 @@ class LoginController {
             mode: "cors",
         })
             .then((response) => {
+                if (!response.ok) throw new Error(response.statusText);
                 return response.json();
             })
             .then((userData) => {
-                const jsWorkouts = userData.workouts
-                    ? userData.workouts.map((value) => dbWorkoutToJS(value))
-                    : [];
-
-                userModel.appUser = new AppUser(
-                    userData.nickname,
-                    password,
-                    jsWorkouts
-                );
-                userModel.appUser.email = userData.email;
+                this.processFetchedUserData(userData, password);
             });
     }
 
+    processFetchedUserData(userData, password) {
+        const jsWorkouts = userData.workouts
+            ? userData.workouts.map((value) => dbWorkoutToJS(value))
+            : [];
+
+        userModel.appUser = new AppUser(
+            userData.nickname,
+            password,
+            jsWorkouts
+        );
+        userModel.appUser.email = userData.email;
+    }
+
     #redirectToUserProfilePage() {
-        location.href = "/#profile-overview";
+        location.hash = "#profile-overview";
+    }
+
+    #handleLoginError(msg) {
+        loginView.clearLoginForm();
+        loginView.showRegistrationErrorInfo(msg);
     }
 }
 export default new LoginController();
--- src/js/view/loginView.js
+++ src/js/view/loginView.js
@@ -27,5 +27,29 @@ class LoginView extends View {
     getLoginFormData() {
         return new FormData(this.#loginForm);
     }
+
+    showRegistrationErrorInfo(msg, callbackOnConfirm) {
+        this._rootElement.insertAdjacentHTML(
+            "afterbegin",
+            `<div class="login-section__login-error error-card m1">
+                <h3>There were some errors while logging in!</h3>
+                <p>${msg ? msg : ""}</p>
+                <button class="button button-primary">Okay</button>
+            </div>`
+        );
+
+        this._rootElement
+            .querySelector(".login-section__login-error button")
+            .addEventListener(
+                "click",
+                callbackOnConfirm
+                    ? callbackOnConfirm
+                    : (e) => this.closeLoginErrorInfo()
+            );
+    }
+
+    closeLoginErrorInfo() {
+        this._removeElementFromThisView(".login-section__login-error");
+    }
 }
 export default new LoginView();
